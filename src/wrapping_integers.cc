c68f4553157ba94f01ee175edc697e7e0d05f28e
1evergreen <cohen_su@icloud.com>
2024-04-07 02:05:57 +0000

    passed Wrap::wrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,18 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  uint32_t val = static_cast<uint32_t>(n + zero_point.raw_value_);
+  return Wrap32(val);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  auto check = Wrap32::wrap(checkpoint, zero_point);
+  uint32_t gap = raw_value_ - check.raw_value_;
+  uint64_t abs_seqno = gap + checkpoint;
+  if(abs_seqno - checkpoint > (1ULL << 31)){
+    return abs_seqno - (1ULL << 32);
+  }
+  return abs_seqno;
 }
