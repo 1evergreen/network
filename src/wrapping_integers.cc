c2f74137e37f5806147b42ae5968b969c5e22865
1evergreen <cohen_su@icloud.com>
2024-04-07 02:15:56 +0000

    passed Wrap::unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,6 +1,9 @@
 #include "wrapping_integers.hh"
 
 using namespace std;
+static const uint64_t UINT_32BIT = (1ULL << 32);
+static const uint64_t UINT_GAP = (1ULL << 31);
+
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
@@ -15,8 +18,8 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   auto check = Wrap32::wrap(checkpoint, zero_point);
   uint32_t gap = raw_value_ - check.raw_value_;
   uint64_t abs_seqno = gap + checkpoint;
-  if(abs_seqno >= (1ULL << 32) && abs_seqno - checkpoint > (1ULL << 31)){
-    return abs_seqno - (1ULL << 32);
+  if(abs_seqno >= UINT_32BIT && abs_seqno - checkpoint > UINT_GAP){
+    return abs_seqno - UINT_32BIT;
   }
   return abs_seqno;
 }
